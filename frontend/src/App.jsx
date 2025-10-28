e385d1a99987c5c013fe1e91e7c4e7ad7b873155
Devansh1010 <devanshprajapati36@gmail.com>
2025-10-28 23:41:28 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Add main application structure with routing for Home, Login, and Register pages.
    - Integrate Tailwind CSS and DaisyUI for styling.
    - Include SVG icons for Vite and React.
    - Create basic components for Home, Login, and Register pages.
    - Set up Vite configuration for React and Tailwind CSS.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Route, Routes, Navigate } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+const App = () => {
+  let authenticated = true // This should be replaced with real authentication logic
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={authenticated ? <Home /> : <Navigate to={'/login'} />}
+      />
+
+      <Route
+        path="/login"
+        element={!authenticated ? < Login /> : <Navigate to={'/'} />}
+      />
+
+      <Route
+        path="/register"
+        element={!authenticated ? <Register /> : <Navigate to={'/'} />} />
+    </Routes>
+  )
+}
+
+export default App
