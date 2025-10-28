8182ff10f5ba66eaebea357c89865d00a971eb32
Devansh1010 <devanshprajapati36@gmail.com>
2025-10-29 02:34:20 +0530

    feat: Implement authentication UI with login and registration forms
    - Updat Aepp component to set authenticated state to false for initial login state.
    - Implement react-hook-form in register and login page
    - Add password visibility toggle and loading state for login submission.
    - Create Register page and Login page with validation.
    - Introduced AuthImagePattern component to display animated code snippets in the background.
    - Add validation schemas for login and registration using zod.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 
 const App = () => {
-  let authenticated = true // This should be replaced with real authentication logic
+  let authenticated = false  // This should be replaced with real authentication logic
   return (
     <Routes>
       <Route
---
+++ frontend/src/utils/validation.js
@@ -0,0 +1,15 @@
+import * as z from 'zod'
+
+export const registerSchema = z.object({
+    name: z.string().min(2, 'Name must be at least 2 characters long'),
+    email: z.email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters long'),
+    // confirmPassword: z.string().min(6, 'Confirm Password must be at least 6 characters long'),
+})
+
+export const loginSchema = z.object({
+    // name: z.string().min(2, 'Name must be at least 2 characters long'),
+    email: z.email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters long'),
+    // confirmPassword: z.string().min(6, 'Confirm Password must be at least 6 characters long'),
+})
